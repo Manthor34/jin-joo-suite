2f21a1284ebac88c41b71450a9df7a6e8c45cc4a
Loke-60000 <ramdani.lokman@gmail.com>
2023-10-05 01:01:17 +0200

    contactform

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -3,65 +3,65 @@ import '../Styles/Contact.css';
 import emailjs from 'emailjs-com';
 import Footer from '../components/MainFooter';
 
-
-function Contact() {
-
-
+const Contact = () => {
     const [nom, setNom] = useState('');
     const [prenom, setPrenom] = useState('');
+    const [email, setEmail] = useState(''); 
     const [objetDuMessage, setObjetDuMessage] = useState('');
     const [message, setMessage] = useState('');
     const [errors, setErrors] = useState({});
     const [isSending, setIsSending] = useState(false);
     const [isSent, setIsSent] = useState(false);
-    const [sendError, setSendError] = useState('');
-    
+
     const handleSubmit = (e) => {
         e.preventDefault();
-    
-        if (!nom || !prenom || !message) {
-        setErrors({
-            nom: !nom && 'Nom is required',
-            prenom: !prenom && 'Prenom is required',
-            message: !message && 'Message is required',
-        });
-        return;
+        setIsSent(false);
+
+        if (!nom || !prenom || !email || !message) {
+            setErrors({
+                nom: !nom && 'Nom is required',
+                prenom: !prenom && 'Prenom is required',
+                email: !email && 'Email is required', 
+                message: !message && 'Message is required',
+            });
+            return;
         }
-    
+
         setIsSending(true);
-    
+
         const templateParams = {
-        from_name: `${prenom} ${nom}`,
-        subject: objetDuMessage || 'New message from your website',
-        message,
+            from_name: `${prenom} ${nom}`,
+            email: email,
+            objetDuMessage: objetDuMessage || 'New message from your website',
+            message,
         };
-    
+
         emailjs
-        .send(
-            window.REACT_APP_EMAILJS_SERVICE_ID,
-            window.REACT_APP_EMAILJS_TEMPLATE_ID,
-            templateParams,
-            window.REACT_APP_EMAILJS_USER_ID
-        )
-        .then(
-            (result) => {
-            console.log(result.text);
-            setIsSending(false);
-            setIsSent(true);
-            setNom('');
-            setPrenom('');
-            setObjetDuMessage('');
-            setMessage('');
-            setErrors({});
-            },
-            (error) => {
-            console.log(error.text);
-            setIsSending(false);
-            setSendError('There was an error sending your email. Please try again later.');
-            }
-        );
+            .send(
+                'service_s7gasem',
+                'template_tkqtuxx',
+                templateParams,
+                'bn0yGwf8fLHDkS3gq'
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    setIsSent(true);
+                    setIsSending(false);
+                    setNom('');
+                    setPrenom('');
+                    setEmail('');
+                    setObjetDuMessage('');
+                    setMessage('');
+                    setErrors({});
+                },
+                (error) => {
+                    console.log(error.text);
+                    setIsSending(false);
+                    setErrors({ sendError: 'There was an error sending your email. Please try again later.' });
+                }
+            );
     };
-    
 
     return (
         <div className="container">
@@ -97,7 +97,21 @@ function Contact() {
                         )}
                     </div>
                     <div className="form-group">
-                        <label htmlFor="objetDuMessage">Objet du Message (optional)</label>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                            id="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        {errors.email && (
+                            <div className="invalid-feedback">{errors.email}</div>
+                        )}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="objetDuMessage">Objet du Message</label>
                         <input
                             type="text"
                             className="form-control"
@@ -124,6 +138,11 @@ function Contact() {
                         {isSending ? 'Sending...' : 'Send'}
                     </button>
                 </form>
+    
+                {isSending && <div style={{ background: '#f0ad4e', padding: '10px', borderRadius: '5px', marginTop: '10px' }}>Envoi en cours...</div>}
+                {errors.sendError && <div style={{ background: 'red', padding: '10px', borderRadius: '5px', marginTop: '10px', color: 'white' }}>{errors.sendError}</div>}
+                {isSent && <div style={{ background: 'green', padding: '10px', borderRadius: '5px', marginTop: '10px', color: 'white' }}>Votre message a été envoyé. Merci!</div>}
+    
                 <hr className='contactHr'/>
                 <p className='contactPara'>communication@woko.fr</p>
                 <p className='contactPara'>12 avenue Antoine Dutrievoz 69100 Villeurbanne</p>
@@ -132,6 +151,6 @@ function Contact() {
         </div>
     );
     
-}
-
+    
+    };
 export default Contact;
